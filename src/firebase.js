323d36bd112f3f257169229aba0e40cdc0616b88
Shalini Gupta <66317415+skgshalini@users.noreply.github.com>
2023-06-08 20:17:47 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-  apiKey: "AIzaSyDyRui9cL0c8XmFV4X1R8JEtMsCkDOd3lU",
-  authDomain: "trainseatbooking-69d90.firebaseapp.com",
-  projectId: "trainseatbooking-69d90",
-  storageBucket: "trainseatbooking-69d90.appspot.com",
-  messagingSenderId: "500562711973",
-  appId: "1:500562711973:web:ba9c796d6d62162218844c"
+ 
 };
 
 // Initialize Firebase
